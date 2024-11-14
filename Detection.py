fd9b1389bd9d35f3fdb86824735fc32adc7b97cd
themrgndz <69049311+themrgndz@users.noreply.github.com>
2024-11-14 08:39:51 +0300

    Update Detection.py

--- Detection.py
+++ Detection.py
@@ -1,22 +1,22 @@
-# Pandas ve NumPy kütüphanelerini ekler
+# Add Pandas and NumPy libraries
 import pandas as pd
 import numpy as np
 
-# Grafik çizimleri için Matplotlib ve Seaborn kütüphanelerini ekler
+# Add Matplotlib and Seaborn libraries for plotting graphs
 import matplotlib.pyplot as plt
 import seaborn as sns
 import csv
 
-# Veri setini eğitim ve test setlerine bölmek için scikit-learn'den ilgili fonksiyonu ekler
+# Add relevant function from scikit-learn to split data into training and test sets
 from sklearn.model_selection import train_test_split
 
-# Veri standardizasyonu için scikit-learn'den ilgili fonksiyonu ekler
+# Add relevant function from scikit-learn for data standardization
 from sklearn.preprocessing import StandardScaler
 
-# Kategorik sütunları sayısal forma çevirme
+# Convert categorical columns to numerical form
 from sklearn.preprocessing import LabelEncoder
 
-# Makine öğrenmesi algoritmaları için scikit-learn'den ilgili sınıfları ekler
+# Add relevant classes from scikit-learn for machine learning algorithms
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
@@ -27,166 +27,166 @@ from sklearn.naive_bayes import BernoulliNB
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import GridSearchCV
 
-# Model performansını değerlendirmek için confusion matrix ve ROC curve ile ilgili fonksiyonları ekler
+# Add functions related to confusion matrix and ROC curve to evaluate model performance
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, roc_curve, auc, confusion_matrix
 
-# .csv dosyasını okuyoruz
+# Read the .csv file
 df = pd.read_csv("Dataset_sdn.csv")
 
-# Her bir sütunun içerdiği eksik verileri çubuk grafiği ile çizdirir
+# Plot missing values in each column as a bar chart
 def plotMissingValues(dataframe):
     missing_values = dataframe.isnull().sum()
     missing_values.plot(kind='bar',color='DarkRed',edgecolor='black')
-    plt.xlabel("Özellikler")
-    plt.ylabel("Eksik değerler")
-    plt.title("Toplam eksik değer")
+    plt.xlabel("Features")
+    plt.ylabel("Missing values")
+    plt.title("Total missing values")
     plt.show()
 
 plotMissingValues(df)
 
-# Etiket (label) sütununda kaç tane 0 ve 1 olduğunu gösteren değerleri alın
+# Get values showing how many 0s and 1s are in the label column
 label_counts = df['label'].value_counts()
 
-# Etiket 0 ve 1 sayılarını ekrana yazdırın
-print("Etiket 0 sayısı:", label_counts[0])
-print("Etiket 1 sayısı:", label_counts[1])
+# Print the number of label 0s and 1s
+print("Number of label 0:", label_counts[0])
+print("Number of label 1:", label_counts[1])
 
-# 0 ve 1 etiketlerinin sayısını eşitlemek için minimum etiket sayısını bulun
+# Find minimum label count to equalize number of 0 and 1 labels
 min_label_count = min(label_counts[0], label_counts[1])
 
-# 0 ve 1 etiketlerinden eşit sayıda örnek alın
+# Take equal number of samples from 0 and 1 labels
 balanced_df = pd.concat([
     df[df['label'] == 0].sample(min_label_count, random_state=42),
     df[df['label'] == 1].sample(min_label_count, random_state=42)
 ])
 
-# Yeni veri setinin etiket sayısını kontrol edin
+# Check label count of new dataset
 balanced_label_counts = balanced_df['label'].value_counts()
 
-# Her bir özelliğin histogram grafiğini oluşturur
+# Create histogram plot for each feature
 plt.figure(figsize=(15, 10))
 for col in balanced_df.columns:
     plt.hist(balanced_df[col],color='DarkBlue',edgecolor='black')
     plt.title(col)
     plt.show()
 
-# Sütunlardaki boşlukları temizler ve sütun isimlerini düzenler
+# Clean spaces in columns and organize column names
 df.columns = df.columns.str.strip()
 
-# Null değerleri içeren satırları siler
+# Delete rows containing null values
 df = df.dropna()
 
-# DataFrame üzerinde NaN (Not a Number) değerlerini kontrol eder
+# Check for NaN (Not a Number) values in DataFrame
 pd.set_option('use_inf_as_na', True)
 null_values = df.isnull().sum()
 
-# Kategorik sütunları one-hot encoding ile çevirin
+# Convert categorical columns with one-hot encoding
 df = pd.get_dummies(df, columns=['src', 'dst'])
 
-#Protocol sütununa labelEncoder uygulanıyor
+# Apply labelEncoder to Protocol column
 df['Protocol'] = LabelEncoder().fit_transform(df['Protocol'])
 
-#label etiketini ayırıp X ve y'yi belirliyoruz
+# Separate label and determine X and y
 X = df.drop('label', axis=1)
 y = df['label']
 
-#Normalizasyon uygulanıyor
+# Apply normalization
 X_normalized = StandardScaler().fit_transform(X)
 
-#train ve test verileri bölünüyor.
+# Split train and test data
 X_train, X_test, y_train, y_test = train_test_split(X_normalized, y, test_size=0.3, random_state=42)
 
-# Confusion matrix çizdirme fonksiyonu
+# Function to plot confusion matrix
 def plot_confusion_matrix(y_true, y_pred, classes, title):
     cm = confusion_matrix(y_true, y_pred)
     plt.figure(figsize=(8, 6))
     sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=classes, yticklabels=classes)
     plt.title(title)
-    plt.xlabel('Tahmin')
-    plt.ylabel('Gerçek')
+    plt.xlabel('Predicted')
+    plt.ylabel('Actual')
     plt.show()
 
 from imblearn.under_sampling import RandomUnderSampler
 from sklearn.metrics import precision_recall_curve, average_precision_score
 
-# RandomUnderSampler nesnesini oluşturun
+# Create RandomUnderSampler object
 rus = RandomUnderSampler(random_state=42)
 
-# Veriyi azaltın
+# Reduce data
 X_train, y_train = rus.fit_resample(X_train, y_train)
 
-# Verileri ölçeklendirme
+# Scale data
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
 #---------------- DDoS Detection ---------------------------------------------------------------------------------------
 
-# Logistic Regression modelini oluşturur ve eğitir
+# Create and train Logistic Regression model
 lr_model = LogisticRegression(random_state=42, max_iter=250)
 lr_model.fit(X_train_scaled, y_train)
 lr_pred = lr_model.predict(X_test_scaled)
 
-# Logistic Regression performans metriklerini hesaplar
+# Calculate Logistic Regression performance metrics
 lr_accuracy = accuracy_score(y_test, lr_pred)
 lr_f1 = f1_score(y_test, lr_pred)
 lr_precision = precision_score(y_test, lr_pred)
 lr_recall = recall_score(y_test, lr_pred)
 
-# 5-katlı çapraz doğrulama
+# 5-fold cross validation
 cv_scores = cross_val_score(lr_model, X_train_scaled, y_train, cv=5)
 
-# Çapraz doğrulama sonuçlarını yazdırma
-print("Çapraz Doğrulama Sonuçları:", cv_scores)
-print("Ortalama Doğruluk:", cv_scores.mean())
+# Print cross validation results
+print("Cross Validation Results:", cv_scores)
+print("Mean Accuracy:", cv_scores.mean())
 
-# Sonuçları yazdırır
+# Print results
 print('\nLogistic Regression Metrics:')
 print(f'Accuracy: {lr_accuracy:.4f}')
 print(f'F1 Score: {lr_f1:.4f}')
 print(f'Precision: {lr_precision:.4f}')
 print(f'Recall: {lr_recall:.4f}')
 
-# Logistic Regression için Confusion Matrix çizdirme
+# Plot Confusion Matrix for Logistic Regression
 plot_confusion_matrix(y_test, lr_pred, ['Normal', 'DDoS'], 'Logistic Regression Confusion Matrix')
 
 #----------------------------------------------------------------------------------------------------------------------
 
-# Decision Tree modelini oluşturur ve eğitir
+# Create and train Decision Tree model
 dt_model = DecisionTreeClassifier(random_state=42)
 dt_model.fit(X_train_scaled, y_train)
 dt_pred = dt_model.predict(X_test_scaled)
 
-# Decision Tree performans metriklerini hesaplar
+# Calculate Decision Tree performance metrics
 dt_accuracy = accuracy_score(y_test, dt_pred)
 dt_f1 = f1_score(y_test, dt_pred)
 dt_precision = precision_score(y_test, dt_pred)
 dt_recall = recall_score(y_test, dt_pred)
 
-# 5-katlı çapraz doğrulama
+# 5-fold cross validation
 cv_scores = cross_val_score(dt_model, X_train_scaled, y_train, cv=5)
 
-# Çapraz doğrulama sonuçlarını yazdırma
-print("Çapraz Doğrulama Sonuçları:", cv_scores)
-print("Ortalama Doğruluk:", cv_scores.mean())
+# Print cross validation results
+print("Cross Validation Results:", cv_scores)
+print("Mean Accuracy:", cv_scores.mean())
 
-# Sonuçları yazdırır
+# Print results
 print('\nDecision Tree Metrics:')
 print(f'Accuracy: {dt_accuracy:.4f}')
 print(f'F1 Score: {dt_f1:.4f}')
 print(f'Precision: {dt_precision:.4f}')
 print(f'Recall: {dt_recall:.4f}')
 
-# Decision Tree için Confusion Matrix çizdirme
+# Plot Confusion Matrix for Decision Tree
 plot_confusion_matrix(y_test, dt_pred, ['Normal', 'DDoS'], 'Decision Tree Confusion Matrix')
 
-# Decision Tree için önemli özellikleri belirleme
+# Determine important features for Decision Tree
 dt_feature_importances = dt_model.feature_importances_
 dt_feature_names = X.columns
 dt_features_df = pd.DataFrame({'Feature': dt_feature_names, 'Importance': dt_feature_importances})
 dt_features_df = dt_features_df.sort_values(by='Importance', ascending=False)
 
-# Decision Tree için önemli özellikleri görselleştirme
+# Visualize important features for Decision Tree
 plt.figure(figsize=(12, 8))
 sns.barplot(x='Importance', y='Feature', data=dt_features_df)
 plt.title('Decision Tree Feature Importance')
@@ -194,35 +194,35 @@ plt.show()
 
 #-------------------------------------------------------------------------------------------------
 
-# Random Forest modelini oluşturur ve eğitir
+# Create and train Random Forest model
 rf_model = RandomForestClassifier(random_state=42)
 rf_model.fit(X_train_scaled, y_train)
 rf_pred = rf_model.predict(X_test_scaled)
 
-# Random Forest performans metriklerini hesaplar
+# Calculate Random Forest performance metrics
 rf_accuracy = accuracy_score(y_test, rf_pred)
 rf_f1 = f1_score(y_test, rf_pred)
 rf_precision = precision_score(y_test, rf_pred)
 rf_recall = recall_score(y_test, rf_pred)
 
-# 5-katlı çapraz doğrulama
+# 5-fold cross validation
 cv_scores = cross_val_score(rf_model, X_train_scaled, y_train, cv=5)
 
-# Çapraz doğrulama sonuçlarını yazdırma
-print("Çapraz Doğrulama Sonuçları:", cv_scores)
-print("Ortalama Doğruluk:", cv_scores.mean())
+# Print cross validation results
+print("Cross Validation Results:", cv_scores)
+print("Mean Accuracy:", cv_scores.mean())
 
-# Sonuçları yazdırır
+# Print results
 print('\nRandom Forest Metrics:')
 print(f'Accuracy: {rf_accuracy:.4f}')
 print(f'F1 Score: {rf_f1:.4f}')
 print(f'Precision: {rf_precision:.4f}')
 print(f'Recall: {rf_recall:.4f}')
 
-# Random Forest için Confusion Matrix çizdirme
+# Plot Confusion Matrix for Random Forest
 plot_confusion_matrix(y_test, rf_pred, ['Normal', 'DDoS'], 'Random Forest Confusion Matrix')
 
-# Random Forest
+# Random Forest feature importance
 feature_importances = rf_model.feature_importances_
 feature_names = X.columns
 features_df = pd.DataFrame({'Feature': feature_names, 'Importance': feature_importances})
@@ -235,133 +235,133 @@ plt.show()
 
 #-------------------------------------------------------------------------------------------------
 
-# SVM modelini oluşturur ve eğitir
+# Create and train SVM model
 svm_model = SVC(random_state=42)
 svm_model.fit(X_train_scaled, y_train)
 svm_pred = svm_model.predict(X_test_scaled)
 
-# SVM performans metriklerini hesaplar
+# Calculate SVM performance metrics
 svm_accuracy = accuracy_score(y_test, svm_pred)
 svm_f1 = f1_score(y_test, svm_pred)
 svm_precision = precision_score(y_test, svm_pred)
 svm_recall = recall_score(y_test, svm_pred)
 
-# 5-katlı çapraz doğrulama
+# 5-fold cross validation
 cv_scores = cross_val_score(svm_model, X_train_scaled, y_train, cv=5)
 
-# Çapraz doğrulama sonuçlarını yazdırma
-print("Çapraz Doğrulama Sonuçları:", cv_scores)
-print("Ortalama Doğruluk:", cv_scores.mean())
+# Print cross validation results
+print("Cross Validation Results:", cv_scores)
+print("Mean Accuracy:", cv_scores.mean())
 
-# Sonuçları yazdırır
+# Print results
 print('\nSVM Metrics:')
 print(f'Accuracy: {svm_accuracy:.4f}')
 print(f'F1 Score: {svm_f1:.4f}')
 print(f'Precision: {svm_precision:.4f}')
 print(f'Recall: {svm_recall:.4f}')
 
-# SVM için Confusion Matrix çizdirme
+# Plot Confusion Matrix for SVM
 plot_confusion_matrix(y_test, svm_pred, ['Normal', 'DDoS'], 'SVM Confusion Matrix')
 
 #-------------------------------------------------------------------------------------------------
 
-# KNN modelini tanımla
+# Define KNN model
 knn_model = KNeighborsClassifier()
 
-# Denenecek k değerleri
+# k values to try
 param_grid = {'n_neighbors': [2, 3, 4, 5, 6, 7, 8, 9, 10]}
 
-# GridSearchCV kullanarak en iyi k değerini bulma
+# Find best k value using GridSearchCV
 grid_search = GridSearchCV(knn_model, param_grid, cv=5, scoring='accuracy')
 grid_search.fit(X_train_scaled, y_train)
 
-# En iyi k değerini yazdırma
+# Print best k value
 best_k_value = grid_search.best_params_['n_neighbors']
-print(f"En iyi k değeri: {best_k_value}")
+print(f"Best k value: {best_k_value}")
 
-# En iyi k değeri ile modeli tekrar eğitme
+# Retrain model with best k value
 best_knn_model = KNeighborsClassifier(n_neighbors=best_k_value)
-best_knn_model.fit(X_train_scaled, y_train)  # En iyi k değeri ile modeli eğit
+best_knn_model.fit(X_train_scaled, y_train)  # Train model with best k value
 
-# Test seti üzerinde performansı değerlendirme
+# Evaluate performance on test set
 best_knn_pred = best_knn_model.predict(X_test_scaled)
 best_knn_accuracy = accuracy_score(y_test, best_knn_pred)
 best_knn_f1 = f1_score(y_test, best_knn_pred)
 best_knn_precision = precision_score(y_test, best_knn_pred)
 best_knn_recall = recall_score(y_test, best_knn_pred)
 
-# En iyi k değeri ile modelin performansını yazdırma
-print('\nEn iyi K-Nearest Neighbors Metrics:')
+# Print performance of model with best k value
+print('\nBest K-Nearest Neighbors Metrics:')
 print(f'Accuracy: {best_knn_accuracy:.4f}')
 print(f'F1 Score: {best_knn_f1:.4f}')
 print(f'Precision: {best_knn_precision:.4f}')
 print(f'Recall: {best_knn_recall:.4f}')
 
-# Confusion Matrix çizdirme
-plot_confusion_matrix(y_test, best_knn_pred, ['Normal', 'DDoS'], 'En iyi KNN Confusion Matrix')
+# Plot Confusion Matrix
+plot_confusion_matrix(y_test, best_knn_pred, ['Normal', 'DDoS'], 'Best KNN Confusion Matrix')
 
 #-------------------------------------------------------------------------------------------------
 
-# MLPClassifier modelini oluşturur ve eğitir
+# Create and train MLPClassifier model
 mlp_model = MLPClassifier(random_state=42, max_iter=300)
 mlp_model.fit(X_train_scaled, y_train)
 
 mlp_pred = mlp_model.predict(X_test_scaled)
 
-# MLPClassifier performans metriklerini hesaplar
+# Calculate MLPClassifier performance metrics
 mlp_accuracy = accuracy_score(y_test, mlp_pred)
 mlp_f1 = f1_score(y_test, mlp_pred)
 mlp_precision = precision_score(y_test, mlp_pred)
 mlp_recall = recall_score(y_test, mlp_pred)
 
-# 5-katlı çapraz doğrulama
+# 5-fold cross validation
 cv_scores_mlp = cross_val_score(mlp_model, X_train_scaled, y_train, cv=5)
 
-# Çapraz doğrulama sonuçlarını yazdırma
-print("MLPClassifier Çapraz Doğrulama Sonuçları:", cv_scores_mlp)
-print("Ortalama Doğruluk:", cv_scores_mlp.mean())
+# Print cross validation results
+print("MLPClassifier Cross Validation Results:", cv_scores_mlp)
+print("Mean Accuracy:", cv_scores_mlp.mean())
 
-# Sonuçları yazdırır
+# Print results
 print('\nMLPClassifier Metrics:')
 print(f'Accuracy: {mlp_accuracy:.4f}')
 print(f'F1 Score: {mlp_f1:.4f}')
 print(f'Precision: {mlp_precision:.4f}')
 print(f'Recall: {mlp_recall:.4f}')
 
-# MLPClassifier için Confusion Matrix çizdirme
+# Plot Confusion Matrix for MLPClassifier
 plot_confusion_matrix(y_test, mlp_pred, ['Normal', 'DDoS'], 'MLPClassifier Confusion Matrix')
 
 #-------------------------------------------------------------------------------------------------
 
-# Bernoulli Naive Bayes modelini oluşturur ve eğitir
+# Create and train Bernoulli Naive Bayes model
 bernoulli_nb_model = BernoulliNB()
 bernoulli_nb_model.fit(X_train_scaled, y_train)
 bernoulli_nb_pred = bernoulli_nb_model.predict(X_test_scaled)
 
-# Bernoulli Naive Bayes performans metriklerini hesaplar
+# Calculate Bernoulli Naive Bayes performance metrics
 bernoulli_nb_accuracy = accuracy_score(y_test, bernoulli_nb_pred)
 bernoulli_nb_f1 = f1_score(y_test, bernoulli_nb_pred)
 bernoulli_nb_precision = precision_score(y_test, bernoulli_nb_pred)
 bernoulli_nb_recall = recall_score(y_test, bernoulli_nb_pred)
 
-# 5-katlı çapraz doğrulama
+# 5-fold cross validation
 cv_scores_bernoulli_nb = cross_val_score(bernoulli_nb_model, X_train_scaled, y_train, cv=5)
 
-# Çapraz doğrulama sonuçlarını yazdırma
-print("Bernoulli Naive Bayes Çapraz Doğrulama Sonuçları:", cv_scores_bernoulli_nb)
-print("Ortalama Doğruluk:", cv_scores_bernoulli_nb.mean())
+# Print cross validation results
+print("Bernoulli Naive Bayes Cross Validation Results:", cv_scores_bernoulli_nb)
+print("Mean Accuracy:", cv_scores_bernoulli_nb.mean())
 
-# Sonuçları yazdırır
+# Print results
 print('\nBernoulli Naive Bayes Metrics:')
 print(f'Accuracy: {bernoulli_nb_accuracy:.4f}')
 print(f'F1 Score: {bernoulli_nb_f1:.4f}')
 print(f'Precision: {bernoulli_nb_precision:.4f}')
 print(f'Recall: {bernoulli_nb_recall:.4f}')
 
-# Bernoulli Naive Bayes için Confusion Matrix çizdirme
+# Plot Confusion Matrix for Bernoulli Naive Bayes
 plot_confusion_matrix(y_test, bernoulli_nb_pred, ['Normal', 'DDoS'], 'Bernoulli Naive Bayes Confusion Matrix')
 
-#---------------- Roc Eğrisi ---------------------------------------------------------------------------------------
+#---------------- ROC Curve ---------------------------------------------------------------------------------------
 
 # Logistic Regression ROC Curve
 lr_prob = lr_model.predict_proba(X_test_scaled)[:, 1]
@@ -383,7 +383,7 @@ svm_prob = svm_model.decision_function(X_test_scaled)
 fpr_svm, tpr_svm, _ = roc_curve(y_test, svm_prob)
 roc_auc_svm = auc(fpr_svm, tpr_svm)
 
-# KNN modelinin tahmin olasılıklarını al
+# Get prediction probabilities for KNN model
 knn_prob = best_knn_model.predict_proba(X_test_scaled)[:, 1]
 fpr_knn, tpr_knn, _ = roc_curve(y_test, knn_prob)
 roc_auc_knn = auc(fpr_knn, tpr_knn)
@@ -393,7 +393,7 @@ nb_prob = bernoulli_nb_model.predict_proba(X_test_scaled)[:, 1]
 fpr_nb, tpr_nb, _ = roc_curve(y_test, nb_prob)
 roc_auc_nb = auc(fpr_nb, tpr_nb)
 
-# ROC Curve çizimi
+# Plot ROC Curve
 plt.figure(figsize=(10, 8))
 plt.plot(fpr_lr, tpr_lr, label=f'Logistic Regression (AUC = {roc_auc_lr:.2f})')
 plt.plot(fpr_dt, tpr_dt, label=f'Decision Tree (AUC = {roc_auc_dt:.2f})')
@@ -437,7 +437,7 @@ knn_avg_precision = average_precision_score(y_test, knn_prob)
 nb_precision, nb_recall, _ = precision_recall_curve(y_test, nb_prob)
 nb_avg_precision = average_precision_score(y_test, nb_prob)
 
-# Precision-Recall Curve çizimi
+# Plot Precision-Recall Curve
 plt.figure(figsize=(10, 8))
 
 # Logistic Regression
@@ -465,8 +465,3 @@ plt.legend(loc='upper right')
 plt.show()
 
 #-------------------------------------------------------------------------------------------------
-
-
-
-
-
